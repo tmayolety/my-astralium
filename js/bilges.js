572d52e0ed2f24208095294d4df0882e7ff1ce69
Tomeu <tomeu.mayol@etechyachting.com>
2024-09-26 10:22:11 +0200

    FIX Bilges and NavLights

--- js/bilges.js
+++ js/bilges.js
@@ -2,3 +2,8 @@ var screen = {
     renderData: function (msg) {
     }
 };
+
+$("#bilgesLayout_1").load("src/ga/Black/Astralium-MainDeck.svg");
+$("#bilgesLayout_2").load("src/ga/Black/Astralium-MainDeck.svg");
+
+
