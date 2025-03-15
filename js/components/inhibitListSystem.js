27ebc6756569a3498390a3d064eaf0119f82a600
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-03-15 13:33:08 +0100

    Rename light headers for clarity and correct signal IDs in navLights.html

--- js/components/inhibitListSystem.js
+++ js/components/inhibitListSystem.js
@@ -15,7 +15,6 @@ components.inhibitListSystem = {
                 
             </header>
 
-
             <div v-if="isLoading" style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw; position: fixed; top: 0; left: 0; z-index: 9999; background-color: rgba(0, 0, 0, 0.5);">
                 <div class="loaderSignals"></div>
             </div>
