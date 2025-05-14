00dfb508e67e7c464347f857d24e4f5be7efca7d
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-05-14 09:34:04 +0200

    test dblswords

--- js/components/basicText.js
+++ js/components/basicText.js
@@ -1,67 +1,58 @@
 components.basicText = {
-  props: ["signalId", "signalIdDB", "unit", "valueMode", "valueDecimals","scalingFactor"],
+  props: ["signalId", "signalIdDB", "unit", "valueMode", "valueDecimals", "scalingFactor"],
+
   template: /*html*/ `
-    <h3 class="ui font-bold h3" ref="basicTextElement">{{ valueToShow }} 
-    <small class="font-regular clr-subvalue-ui">{{ unit }}</small>
-    <div style="">{{ value }}</div>
-    <div style="">{{ dbValue }}</div>
+    <h3 class="ui font-bold h3" ref="basicTextElement">
+      {{ valueToShow }} 
+      <small class="font-regular clr-subvalue-ui">{{ unit }}</small>
+      <div style="">LSW: {{ lsw }}</div>
+      <div style="">MSW: {{ dbValue }}</div>
     </h3>`,
 
   data() {
     return {
-      value: null,
+      lsw: null,               // Nuevo: valor crudo LSW
       valueToShow: null,
       combined: null,
       decimals: typeof this.valueDecimals == "undefined" ? 0 : this.valueDecimals,
     };
   },
-    computed: {
+
+  computed: {
     dbValue() {
-      return valueRaw[this.signalIdDB]; // ← se evalúa dinámicamente por instancia
+      return valueRaw[this.signalIdDB]; // MSW dinámico
     }
   },
-
 
   mounted() {
     switch (this.valueMode) {
       case "filtered":
-        this.value = valueFiltered[this.signalId];
+        this.lsw = valueFiltered[this.signalId];
         break;
       case "escalated":
-        this.value = valueEscalated[this.signalId];
+        this.lsw = valueEscalated[this.signalId];
         break;
       default:
       case "raw":
-        this.value = valueRaw[this.signalId];
+        this.lsw = valueRaw[this.signalId];
         break;
     }
 
-    if (this.value == null) {
+    if (this.lsw == null) {
       this.valueToShow = "N/A";
     }
-
-    if (typeof this.valueDecimals === "undefined") {
-      this.decimals = 0;
-    } else {
-      this.decimals = this.valueDecimals;
-    }
-
   },
+
   updated() {
-     const dbVal = valueRaw[this.signalIdDB]; // ← accede dinámicamente aquí también
+    const msw = this.dbValue;
+    const lsw = this.lsw;
+    const scale = this.scalingFactor ?? 100;
 
-    if (this.signalIdDB && !isNaN(dbVal) && !isNaN(this.value)) {
-      const msw = dbVal;
-      const lsw = this.value;
-      const scale = this.scalingFactor ?? 100;
+    if (this.signalIdDB && !isNaN(msw) && !isNaN(lsw)) {
       this.combined = ((msw << 16) | lsw) / scale;
-
-      this.value = this.combined;
       this.valueToShow = this.combined.toFixed(this.decimals);
-    } 
-    else if (!isNaN(this.value)) {
-      this.valueToShow = parseFloat(this.value).toFixed(this.decimals);
+    } else if (!isNaN(lsw)) {
+      this.valueToShow = parseFloat(lsw).toFixed(this.decimals);
     }
-  },
-
+  }
 };
